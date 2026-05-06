e97a35044552609dc0df683e37d96ec9a169108e
Juan Alcantara <jalcanmi@mercadona.es>
2026-05-06 14:28:53 +0200

    feat: Add paddles

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use rand::prelude::*;
 struct Player;
 
 #[derive(Component)]
+struct Paddle;
+
+#[derive(Component)]
 struct MainCamera;
 
 #[derive(Component)]
@@ -18,12 +21,32 @@ struct Ball;
 struct BallConfig {
     speed: f32,
     size: f32,
+    mass: f32,
+    bounciness: f32,
+}
+
+#[derive(Resource)]
+struct PaddleConfig {
+    height: f32,
+    width: f32,
+    padding: f32,
 }
 
 fn main() {
     let mut app = App::new();
 
     app.add_plugins(DefaultPlugins)
+        .insert_resource(BallConfig {
+            speed: 150.0,
+            size: 2.5,
+            mass: 0.56,
+            bounciness: 1.0,
+        })
+        .insert_resource(PaddleConfig {
+            height: 20.0,
+            width: 5.0,
+            padding: 5.0,
+        })
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .add_plugins(RapierDebugRenderPlugin::default())
         .add_systems(Startup, setup)
@@ -34,6 +57,8 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    ball_config: Res<BallConfig>,
+    paddle_config: Res<PaddleConfig>,
 ) {
     let mut rng = rand::rng();
     commands
@@ -67,23 +92,53 @@ fn setup(
         .insert(MeshMaterial2d(materials.add(Color::WHITE)))
         .insert(Transform::from_xyz(0., 125., 0.));
 
-    let launch_ball_right = rng.random_bool(0.5);
-    let launch_impulse_dir = if launch_ball_right {
+    let launch_impulse = if rng.random_bool(0.5) {
         Vec2::new(1., 0.)
     } else {
         Vec2::new(-1., 0.)
-    };
+    } * ball_config.speed;
     commands
         .spawn(Ball)
         .insert(RigidBody::Dynamic)
         .insert(GravityScale(0.))
         .insert(ExternalImpulse {
-            impulse: launch_impulse_dir * 40.,
+            impulse: launch_impulse,
             torque_impulse: 0.,
         })
-        .insert(Collider::ball(2.5))
-        .insert(ColliderMassProperties::Mass(0.56))
-        .insert(Mesh2d(meshes.add(Circle::new(2.5))))
+        .insert(Restitution {
+            coefficient: ball_config.bounciness,
+            combine_rule: CoefficientCombineRule::Max,
+        })
+        .insert(Collider::ball(ball_config.size))
+        .insert(ColliderMassProperties::Mass(ball_config.mass))
+        .insert(Mesh2d(meshes.add(Circle::new(ball_config.size))))
         .insert(MeshMaterial2d(materials.add(Color::WHITE)))
         .insert(Transform::from_xyz(0., 0., 0.));
+
+    commands
+        .spawn(Paddle)
+        .insert(RigidBody::KinematicPositionBased)
+        .insert(Collider::cuboid(
+            paddle_config.width / 2.,
+            paddle_config.height / 2.,
+        ))
+        .insert(Mesh2d(
+            meshes.add(Rectangle::new(paddle_config.width, paddle_config.height)),
+        ))
+        .insert(MeshMaterial2d(materials.add(Color::WHITE)))
+        .insert(Transform::from_xyz(paddle_config.padding - 250.0, 0., 0.));
+
+    commands
+        .spawn(Player)
+        .insert(Paddle)
+        .insert(RigidBody::KinematicPositionBased)
+        .insert(Collider::cuboid(
+            paddle_config.width / 2.,
+            paddle_config.height / 2.,
+        ))
+        .insert(Mesh2d(
+            meshes.add(Rectangle::new(paddle_config.width, paddle_config.height)),
+        ))
+        .insert(MeshMaterial2d(materials.add(Color::WHITE)))
+        .insert(Transform::from_xyz(250.0 - paddle_config.padding, 0., 0.));
 }
